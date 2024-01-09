d9d1be4c166b595d07a7afe19daf9f3e6c79701c
Daniil Sergeev <daniil.scorpio@gmail.com>
2024-01-09 14:39:29 +0300

    refactor: delete old code

--- controllers/index.js
+++
@@ -1,4 +0,0 @@
-export * as UserController from './UserController.js';
-export * as CompanyInfoController from './CompanyInfoController.js';
-export * as OrdersController from './OrdersController.js';
-export * as OrderStatusesController from './OrderStatusesController.js';
